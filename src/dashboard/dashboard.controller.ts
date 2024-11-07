0b7f37b62f6ed723b2d2a32d2fcb7a4e45bc4915
ngrwisnu <adiprayogongrwisnu@gmail.com>
2024-11-07 17:22:36 +0700

    set up method-override library

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Put, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('dashboard')
 export class DashboardController {
   @Get()
   @Render('dashboard/index.ejs')
   async viewDashboard() {}
+
+  @Put('update/portfolio')
+  async updatePortfolio(@Res() res: Response) {
+    res.status(200).json({
+      message: 'OK',
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as methodOverride from 'method-override';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+  app.use(methodOverride('_method'));
 
   await app.listen(3000, () => {
     console.log('Running on port 3000');
