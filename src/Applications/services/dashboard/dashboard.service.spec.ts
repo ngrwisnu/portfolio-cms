851d3657acdb9c2407f79681ca3e5fa385f8d744
ngrwisnu <adiprayogongrwisnu@gmail.com>
2024-11-19 12:44:15 +0700

    add dashboard service test  - should trigger testing job

--- src/Applications/services/dashboard/dashboard.service.spec.ts
+++ src/Applications/services/dashboard/dashboard.service.spec.ts
@@ -1,18 +1,36 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import StorageRepository from '../../../Domains/storage/storage.repository';
 import { DashboardService } from './dashboard.service';
 
+class MockStorageRepository extends StorageRepository<string> {
+  add = jest.fn().mockImplementation(() => Promise.resolve({ status: 'ok' }));
+  get = jest.fn();
+  delete = jest.fn();
+}
+
 describe('DashboardService', () => {
-  let service: DashboardService;
+  let storageRepository: MockStorageRepository;
+  let dashboardService: DashboardService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [DashboardService],
-    }).compile();
-
-    service = module.get<DashboardService>(DashboardService);
+  beforeEach(() => {
+    storageRepository = new MockStorageRepository();
+    dashboardService = new DashboardService(storageRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should call the add method from storage repository', async () => {
+    await dashboardService.add('');
+
+    expect(storageRepository.add).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call the get method from storage repository', async () => {
+    await dashboardService.get();
+
+    expect(storageRepository.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call the remove method from storage repository', async () => {
+    await dashboardService.remove('');
+
+    expect(storageRepository.delete).toHaveBeenCalledTimes(1);
   });
 });
